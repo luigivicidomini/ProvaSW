2decce143629342914103f3da6a4854f5ef1cb33
luigi <luigi@omen.lan>
2021-11-09 17:32:26 +0100

    Correzione errore grammaticale

--- Prova/src/main/java/com/mycompany/prova/Main.java
+++ Prova/src/main/java/com/mycompany/prova/Main.java
@@ -10,6 +10,6 @@ package com.mycompany.prova;
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, wolrd!");
+        System.out.println("Hello, world!");
     }
 }
