4471bbd063d11edc12f706a785d6e03930b90960
luigi <luigi@omen.lan>
2021-11-09 17:20:46 +0100

    Secondo commit

--- Prova/src/main/java/com/mycompany/prova/Main.java
+++ Prova/src/main/java/com/mycompany/prova/Main.java
@@ -11,5 +11,6 @@ package com.mycompany.prova;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello, wolrd!");
+        System.out.println("Hello, universe!");
     }
 }
