862656e50bc3d16346228c4a04426a7a24efcbeb
luigi <luigi@omen.lan>
2021-11-09 17:23:15 +0100

    Ripristino prima versione

--- Prova/src/main/java/com/mycompany/prova/Main.java
+++ Prova/src/main/java/com/mycompany/prova/Main.java
@@ -11,6 +11,5 @@ package com.mycompany.prova;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello, wolrd!");
-        System.out.println("Hello, universe!");
     }
 }
